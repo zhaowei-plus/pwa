decbe80513b3f3e2a35cfb742bbf01e5f9b0667a
zhaowei-plus <1215058679@qq.com>
2019-08-07 22:17:33 +0800

    初始化

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('dist'));
+
+//监听端口为3000
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
